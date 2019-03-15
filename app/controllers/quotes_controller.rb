0476c3eeb8f82afc767e7a69f5c225843919b752
wamblejosh <wamblejosh@gmail.com>
2019-03-15 22:18:55 +0000

    Inserting quotes

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,4 +1,5 @@
 class QuotesController < ApplicationController
 	def index
-	end	
+      @quote = Quote.order("RANDOM()").first
+    end
 end
--- db/migrate/20190315212655_create_quotes.rb
+++ db/migrate/20190315212655_create_quotes.rb
@@ -1,9 +1,11 @@
-class CreateQuotes < ActiveRecord::Migration[5.2]
+class CreateQuotes < ActiveRecord::Migration[5.0]
+  
   def change
-    create_table :quotes do |t|
-    	t.string :saying
-    	t.string :author 
-    	t.timestamps
-    end
+  
+     create_table :quotes do |t|
+    	 t.string :saying
+    	 t.string :author 
+    	 t.timestamps
+        end
   end
 end
