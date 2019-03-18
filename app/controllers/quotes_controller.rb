55062d262deaeed5b548157e3c7ebeae72638b8d
wamblejosh <wamblejosh@gmail.com>
2019-03-18 15:59:39 +0000

    fixing unforseen error

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,24 +2,24 @@ class QuotesController < ApplicationController
   def index
     @quote = Quote.order("RANDOM()").first
   end
+  
+  def create
+    @quote = Quote.create(quote_params)
+    if @quote.invalid?
+      flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
+    end
+    redirect_to root_path
+  end
 
-   def create
-     @quote = Quote.create(quote_params)
+  def about
+  end
 
-     if @quote.invalid?
-        flash[:error] = '<strong>Could not save</strong> the data that was entered is invalid.'
-     end   
-       redirect_to root_path
-   end 
+  private
 
-   def about
-
-   end 
-
-   private
-
-   def quote_params
-     params.require(:quote).permit(:saing, :author)
-   end 
-
+  def quote_params
+    params.require(:quote).permit(:saying, :author)
+  end
 end
+ 
+ 
+ 
