1e7d4b51b7230d36d14fea9be3b8150009fa7e84
wamblejosh <wamblejosh@gmail.com>
2019-03-18 00:12:31 +0000

    learning val

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,9 +1,27 @@
 class QuotesController < ApplicationController
-	def index
-      @quote = Quote.all.sample
-    end
+  def index
+    @quote = Quote.order("RANDOM()").first
+  end
 
-    def new
-    	@quote = Quote.new
-    end 	
+   def new 
+
+    @quote = Quote.new
+
+   end
+
+   def create
+     @quote = Quote.create(quote_params)
+
+     if @quote.invalid?
+        flash[:error] = '<strong>Could not save</strong> the data that was entered is invalid.'
+     end   
+       redirect_to root_path
+   end 
+
+   private
+
+   def quote_params
+     params.require(:quote).permit(:saing, :author)
+   end 
+
 end
